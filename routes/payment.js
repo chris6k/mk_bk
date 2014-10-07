96fc23a8d85a018c5968c0b30cce889d17f771cd
kun.xue <kun.xue@buzzinate.com>
2014-10-08 00:24:16 +0800

    add async lib for payment and task saving work flow.

--- routes/payment.js
+++ routes/payment.js
@@ -6,58 +6,135 @@ var router = express.Router();
 var db = require('../db/mysql');
 var auth = require('./authorization').authorize;
 var data = require("../data_service/data");
+var async = require("async");
+var val = require("validator");
 
-router.post('/ask', auth, function (req, res) {
+var checkPaymentParam = function (req, res, next) {
     try {
         var uid = req.get("mk_id");
         var account = req.param("account");
-        var payment = req.param("payment") * 100;
+        var payment = req.param("payment");
         var paymentType = req.param("paymentType");
-    } catch (e) {
-        console.log(e);
-        res.json(400, {result: false, msg: "未知的参数"});
-        return;
-    }
-    db.getBalanceByUid(uid, function (err, rows, fields) {
-        var balance = rows.length == 1 ? rows[0].balance : 0;
-        var ret = {result: false, data: "发生错误"};
-        //账户金额小于10元不能取现。
-        if (balance < 1000) {
-            ret.data = "账户余额不足10元，未满足最低取现额度";
-            res.json(200, ret);
-        } else if (balance >= payment) {
-            db.askPayment(uid, account, payment, paymentType, function (err, rows, fields) {
-                    if (err) {
-                        res.json(200, ret);
-                    } else {
-                        if (!err) {
-                            db.updateBalanceByUid(uid, -payment, function (err, rows, fields) {
-                                    if (!err) {
-                                        ret.result = true;
-                                        ret.data = rows.insertId;
-                                        res.json(200, ret);
-                                        data.addPayment(
-                                            {userId: uid, payment: payment, description: paymentType == 1 ? "手机充值" :
-                                                "支付宝" + "取现" + payment / 100 + "元",
-                                                balance: balance - payment
-                                            }
-                                        );
-                                    } else {
-                                        res.json(200, ret);
-                                    }
-                                }
-                            );
-                        } else {
-                            res.json(200, ret);
-                        }
-                    }
-                }
-            );
-        } else {
-            res.json(200, {result: false, data: "超过可取现额度"});
+        if (val.isNumeric(uid)
+            && val.isNumeric(payment)
+            && account && paymentType) {
+            next();
+            return;
         }
+    } catch (e) {
+        console.error(e);
+    }
+    res.status(400).json({result: false, msg: "未知的参数"});
+}
+
+/**
+ * 获取当前余额
+ * @param ret
+ * @param uid
+ * @param payment
+ * @returns {Function}
+ */
+var getBalance = function (ret, uid, payment) {
+    return function (callback) {
+        db.getBalanceByUid(uid, function (err, rows, fields) {
+            var balance = rows.length == 1 ? rows[0].balance : 0;
+            //账户金额小于10元不能取现。
+            if (balance < 1000) {
+                ret.data = "账户余额不足10元，未满足最低取现额度";
+                callback("balance less than 10 yuan", ret);
+            } else if (balance < payment) {
+                ret.data = "超过余额";
+                callback("balance less than payment", ret);
+            } else {
+                callback(null, balance);
+            }
+        })
+    }
+};
+
+/**
+ * 更新余额
+ * @param ret
+ * @param uid
+ * @param payment
+ * @returns {Function}
+ */
+var updateBalance = function (ret, uid, payment) {
+    return function (balance, callback) {
+        db.updateBalanceByUid(uid, -1 * payment, function (err, rows, fields) {
+                if (!err) {
+                    callback(null, balance);
+                } else {
+                    ret.data = "更新余额失败";
+                    callback("db err", ret);
+                }
+            }
+        );
+    }
+}
+
+/**
+ * 添加支付记录
+ * @param ret
+ * @param uid
+ * @param account
+ * @param payment
+ * @param paymentType
+ * @returns {Function}
+ */
+var askPayment = function (ret, uid, account, payment, paymentType) {
+    return function (balance, callback) {
+        db.askPayment(uid, account, payment, paymentType, function (err, rows, fields) {
+                if (err) {
+                    ret.data = "添加支付记录失败";
+                    callback("db err", ret);
+                } else {
+                    callback(null, balance);
+                }
+            }
+        );
+    }
+};
+
+/**
+ * 添加统计信息
+ * @param ret
+ * @param uid
+ * @param payment
+ * @param paymentType
+ * @returns {Function}
+ */
+var addPayment = function (ret, uid, payment, paymentType) {
+    return function (balance, callback) {
+        data.addPayment(
+            {userId: uid, payment: payment, description: paymentType == 1 ? "手机充值" :
+                "支付宝" + "取现" + payment / 100 + "元",
+                balance: balance - payment
+            }
+        );
+        ret.result = true;
+        ret.data = balance - payment;
+        callback(null, ret);
+    }
+}
+
+
+router.post('/ask', auth, checkPaymentParam, function (req, res) {
+    var ret = {result: false, data: "发生错误"};
+    var uid = req.param("uid");
+    var payment = req.param("payment") * 100;
+    var account = req.param("account");
+    var paymentType = req.param("paymentType");
+
+
+    async.waterfall([
+        getBalance(ret, uid, payment),
+        askPayment(ret, uid, account, payment, paymentType),
+        updateBalance(ret, uid, payment),
+        addPayment(ret, uid, payment, paymentType)
+    ], function (err, result) {
+        res.json(200, result);
     });
-})
-;
+});
 
 module.exports = router;
--- routes/task.js
+++ routes/task.js
@@ -6,47 +6,104 @@ var router = express.Router();
 var db = require('../db/mysql');
 var auth = require('./authorization').authorize;
 var data = require("../data_service/data");
+var async = require("async");
+var val = require("validator");
+var checkTaskParam = function (req, res, next) {
+    try {
+        var userId = req.param("userId");
+        var platformId = req.param("platformId");
+        var taskId = req.param("taskId");
+        var amount = req.param("amount");
+        var taskName = req.param("taskName");
+        if (!val.isNumeric(userId) || !val.isNumeric(platformId)
+            || !taskId || !val.isNumeric(amount) || !taskName) {
+            var ret = {result: false, data: "错误的参数"};
+            res.json(400, ret);
+        } else {
+            next();
+        }
+    } catch (e) {
+        console.error(e);
+        res.json(500, {result: false, data: "未知的错误"});
+    }
+};
 
-router.post('/finish', auth, function (req, res) {
+var getBalance = function (ret, userId) {
+    return function (callback) {
+        try {
+            db.getBalanceByUid(userId, function (err, rows, fields) {
+                if (!err) {
+                    var balance = rows[0].balance || 0;
+                    callback(null, balance);
+                } else {
+                    callback("db err", ret)
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+};
+
+var saveFinishTask = function (ret, userId, platformId, taskId, amount, taskName) {
+    return function (callback) {
+        db.saveFinishTask(userId, platformId, taskId, amount,
+            taskName, function (err, rows, result) {
+                if (err) {
+                    ret.data = "发生错误";
+                    callback("db err", ret);
+                } else {
+                    if (rows.insertId > 0) {
+                        callback(null);
+                    }
+                }
+            });
+    }
+};
+
+var updateBalance = function (ret, userId, amount) {
+    return function (callback) {
+        db.updateBalanceByUid(userId, amount, function (err, rows, fields) {
+            if (!err) {
+                callback(null);
+            } else {
+                ret.data = "update balance err";
+                callback("db err", ret);
+            }
+        });
+    }
+};
+
+var updateStat = function (ret, userId, amount, taskId, taskName, platformId) {
+    return function (balance, callback) {
+        data.addTask({userId: userId, amount: amount, taskId: taskId,
+            taskName: taskName, platformId: platformId, balance: parseInt(balance, 0)});
+        ret.result = true;
+        ret.data = "操作成功";
+        callback(null, ret);
+    }
+};
+
+
+router.post('/finish', checkTaskParam, auth, function (req, res) {
     var userId = req.param("userId");
     var platformId = req.param("platformId");
     var taskId = req.param("taskId");
     var amount = req.param("amount");
     var taskName = req.param("taskName");
-    db.getBalanceByUid(userId, function (err, rows, fields) {
-        if (!err) {
-            var balance = rows[0].balance || 0;
-
-            db.saveFinishTask(userId, platformId, taskId, amount,
-                taskName, function (err, rows, result) {
-                    var ret = {result: false, data: null};
-                    if (err) {
-                        ret.data = "发生错误";
-                    } else {
-                        if (rows.insertId > 0) {
-                            //更新用户account表
-                            db.updateBalanceByUid(userId, amount, function (err, rows, fields) {
-                                if (!err) {
-                                    ret.result = true;
-                                    ret.data = rows.insertId;
-                                } else {
-                                    console.error(err);
-                                }
-                                res.json(200, ret);
-                            });
-                            data.addTask({userId: userId, amount: amount, taskId: taskId,
-                                taskName: taskName, platformId: platformId, balance: parseInt(balance, 0) + parseInt(amount, 0)});
-                            return;
-                        } else {
-                            ret.data = 0;
-                        }
-                    }
-                    res.json(200, ret);
-                });
-        } else {
-            res.json(400, {result: false, data: "未知的用户"});
+    var ret = {result: false, data: null};
+    async.waterfall([
+        saveFinishTask(ret, userId, platformId, taskId, amount, taskName),
+        updateBalance(ret, userId, amount),
+        getBalance(ret, userId),
+        updateStat(ret, userId, amount, taskId, taskName, platformId)
+    ], function (err, result) {
+        try {
+            res.json(200, result);
+        } catch (e) {
+            console.error(e);
         }
-    });
+    })
 });
 
 module.exports = router;
